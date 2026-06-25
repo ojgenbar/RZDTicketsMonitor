c42d6727539fdde85902010f84ae954b18c8b428
Evgeniy Baryshkov <e.baryshkov@gmail.com>
2026-06-26 00:05:58 +0100

    refactor contexts

--- app/bot.py
+++ app/bot.py
@@ -1,3 +1,5 @@
+import contextlib
+
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
@@ -18,17 +20,19 @@ storage = MemoryStorage()
 dispatcher = Dispatcher(bot, storage=storage)
 messengers = {}
 rzd_client: client.RZDClient | None = None
+_lifespan: contextlib.AsyncExitStack | None = None
 
 routes.apply_routes(dispatcher)
 
 
 async def on_startup(_):
-    global rzd_client
-    rzd_client = client.RZDClient()
-    await rzd_client.__aenter__()
+    global rzd_client, _lifespan
+    _lifespan = contextlib.AsyncExitStack()
+    rzd_client = await _lifespan.enter_async_context(client.RZDClient())
 
 
 async def on_shutdown(_):
-    global rzd_client
-    if rzd_client:
-        await rzd_client.__aexit__(None, None, None)
+    global _lifespan
+    if _lifespan:
+        await _lifespan.aclose()
+        _lifespan = None
